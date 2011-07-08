7110994e45b2b05815befdeb53efca75d6911ddb
Ujlaky Tibor <tujlaky@adverticum.com>
2011-07-08 15:59:11 +0200

    added support for create combobox from ajax

--- javascript/jquery.ComboBox.js
+++ javascript/jquery.ComboBox.js
@@ -6,38 +6,70 @@
  */
 (function($) {
 	/* i18n */
-	$.fn.ComboBox = function() {}
- 	$.fn.ComboBox.regional = new Array();
-	$.fn.ComboBox.setRegional = function($lang) {
-		$.extend($.fn.ComboBox.regional["default"], $lang);
+	$.fn.CBox = function() {}
+ 	$.fn.CBox.regional = new Array();
+	$.fn.CBox.setRegional = function($lang) {
+		$.extend($.fn.CBox.regional["default"], $lang);
 	}
 
-	$.fn.ComboBox.regional["default"] = {
+	$.fn.CBox.defaults = {
+		regional: "default",
+		ajax: false,
+		displayField: "name",
+		valueField: "id",
+		template: null,
+		url: null,
+		lineId: "combobox-item-{i}"
+	}
+
+	$.fn.CBox.options = new Array();
+
+	$.fn.CBox.regional["default"] = {
 		notfound: "Pattern not found",
 		inputempty: "Select a value ..."
 	}
 	/* end of i18n */
 
  	function ComboBox() {
+		var _self = this;
+
 		// Main variables
 		/* timer of search field */
 		this.timer = null;
 		/* elements of select*/
 		this.Elements = Array();
+
+		this.divs = Array();
 		/* mouse state */
 		this.mouse_enabled = true;
 		/*div for implement box*/
 		this.$select_div = null;
-		this.$select = null;
-		var _self = this;
+		this.$input = null;
+		this.original_options = null;
 		
 		this.changeSelect = function(selected) {
-			_self.$select.find("option").each(function() {
-				if ($(this).val() == selected) {
-					$(this).attr("selected", true);
+			var callback = $.fn.CBox.options["afterChange"];
+			var new_val;
+
+			if (_self.type == "select") {
+				_self.$input.find("option").each(function() {
+					if ($(this).val() == selected) {
+						$(this).attr("selected", true);
+						new_val = $(this).val();
+					}
+				});
+				_self.$input.trigger("change");
+			} else if (_self.type == "hidden") {
+				var reg = new RegExp($.fn.CBox.options["lineId"]+"-");
+				var val = selected.replace(reg, "");
+				if (val != null) {
+					_self.$input.val(val);
+					new_val = val;
 				}
-			});
-			_self.$select.trigger("change");
+			}
+			if (callback != null) {
+				callback(new_val);
+			}
 		}
 
 		this.showFields = function() {
@@ -60,9 +92,17 @@
 	
 		}
 
+		this.resetFields = function() {
+			var $options_div = _self.$select_div.find("div.option_fields");
+			
+			$options_div.empty();
+			$options_div.html(_self.original_options);
+		}
+
 		this.hideFields = function () {
 			var $fields = _self.$select_div.find("div.option_fields");
 			$fields.hide();
+			_self.resetFields();
 			_self.$select_div.css("z-index", 1);
 			$fields.addClass("inactive_fields");
 		}
@@ -78,7 +118,11 @@
 			for (i=0; i<this.Elements.length; i++) {
 				if (matcher.test(this.Elements[i])) {
 					var match = this.Elements[i].match(reg);
-					results[j] = match[1]+"<span class='matched'>"+match[2]+"</span>"+match[3];
+					var text = match[1]+"<span class='matched'>"+match[2]+"</span>"+match[3];
+					results[j] = this.divs[i].clone();
+
+					results[j].html(text);
+								
 					j++;
 				}
 			}
@@ -100,15 +144,15 @@
 			if (results.length == 0) {
 				$(document.createElement("span"))
 					.addClass("message")
-					.html($.fn.ComboBox.regional["default"].notfound)
+					.html($.fn.CBox.regional["default"].notfound)
 					.appendTo($options_div);
 			}
 
 			for (i=0; i<results.length; i++) {
-				$item = $(document.createElement("div"))
-					.addClass("combobox_item")
-					.html(results[i])
-					.appendTo($options_div);
+
+				$item = results[i];
+				
+				$item.appendTo($options_div);
 
 				if (i==0) {
 					$item.addClass("hovered-item").addClass("scrolled_with_keyboard");
@@ -188,9 +232,17 @@
 			if (e.keyCode == 13) {
 				_self.$select_div.find("div.selected").removeClass("selected");
 				_self.$select_div.find("div.hovered-item").addClass("selected").removeClass("hovered-item");
-				var selected = _self.$select_div.find("div.selected").text();
-				_self.changeSelect(selected);
-				$(this).val(selected);
+				var new_val = _self.$select_div.find("div.selected").text();
+
+
+				if (_self.type == "select") {
+					_self.changeSelect(new_val);
+				} else if (_self.type == "hidden") {
+					var selected = _self.$select_div.find("div.selected").attr("id");
+					_self.changeSelect(selected);
+				}
+
+				$(this).val(new_val);
 				_self.hideFields();
 				var $next = _self.$select_div.parent().next();
 				var $next_input = $next.find("div.combobox_container > input.combobox_input");
@@ -273,21 +325,100 @@
 			}
 			return true;
 		}
+
+		this.createComboBox = function() {
+			var $div   = $(document.createElement("div")).addClass("combobox_container");
+			var $input = $(document.createElement("input"))
+						.addClass("combobox_input")
+						.addClass("empty")
+						.appendTo($div)
+						.val($.fn.CBox.regional["default"].inputempty)
+						.attr("autocomplete", "off");
+
+
+			var $button = $(document.createElement("a"))
+						.addClass("open_box_button")
+						.appendTo($div)
+						.attr("href", "#");
+			var $fields = $(document.createElement("div"))
+						.addClass("option_fields")
+						.addClass("inactive_fields")
+						.appendTo($div);
+
+			var options = $.fn.CBox.options;
+
+			if (options["ajax"] == true) {
+				if (options["url"] != null) {
+					$.ajax ({
+						url: options["url"],
+						type: "POST",
+						dataType: "json",
+						async: false,
+						success: function(data) {
+							// create lines
+							var dataIndex = $.fn.CBox.options["dataIndex"];
+							var mydata;
+
+							if (dataIndex != null) {
+								mydata = data[dataIndex];
+							} else {
+								mydata = data;
+							}
+							for (var i in mydata) {
+								if ($.fn.CBox.options["template"] != null) {
+									var template = $.fn.CBox.options["template"];
+
+									var matches = $.fn.CBox.options["template"].match(/{(.*?)}/g);
+				
+									for (var j in matches) {
+										var index = matches[j].replace(/{(.*)}/, "$1");
+										var val = mydata[i][index];
+										if (index != null && val != null) {
+											var reg = new RegExp("{"+index+"}");
+											template = template.replace(reg, val);
+										}
+
+									}
+
+									$fields.append(template);
+								} else {
+									var $option = $(document.createElement("div"))
+										.addClass("combobox_item")
+										.appendTo($fields)
+										.html(display);
+								}
+							}
+						}
+					});
+				}	
+			
+			}
+
+			return $div;
+		}
+
 		/*
 		 * Create combobox
 		 */
-		this.initComboBox = function($select) {
-			$select.hide();
+		this.initComboBox = function($input) {
 			var $select_div = this.$select_div;
+			if (_self.type == "select") {
+				$input.hide();
+				$select_div.insertAfter($input);
+			} else {
+				$select_div.appendTo($("body"));
+			}
 
-			$select_div.insertAfter($select);
 			var i=0;
 			var combobox = this;
-			$select_div.find("div.option_fields > div.combobox_item").each(function() {
+			var $options_field =  $select_div.find("div.option_fields");
+
+			_self.original_options = $options_field.html();	
+			$options_field.find(" > div.combobox_item").each(function() {
 				combobox.Elements[i] = $(this).text();
+				combobox.divs[i]     = $(this);
 				i++;
 			});
-			
 			this.scroller($select_div.find("div.select"));
 			$select_div.find("input.combobox_input").bind("keydown", this.arrowPressed );
 			$select_div.find("input.combobox_input").bind("keypress", this.enterPressed );
@@ -318,7 +449,12 @@
 				$(this).addClass("selected");
 				$select_div.find("input.combobox_input").val($(this).text());
 				_self.hideFields();
-				_self.changeSelect($(this).text());
+				if (_self.type == "select") {
+					_self.changeSelect($(this).text());
+				} else if (_self.type == "hidden") {
+					_self.changeSelect($(this).attr("id"));
+				}
+
 			});
 			$select_div.find("option_fields").bind("keydown", function(e) {
 				var $input = $(this).find("input.combobox_input");
@@ -380,11 +516,13 @@
 	createComboBoxFromSelect = function($select) {
 		var $div   = $(document.createElement("div")).addClass("combobox_container");
 		var $input = $(document.createElement("input"))
+					.attr("type", "text")
 					.addClass("combobox_input")
 					.addClass("empty")
-					.appendTo($div)
-					.val($.fn.ComboBox.regional["default"].inputempty)
-					.attr("autocomplete", "off");
+					.val("asd")
+					.attr("autocomplete", "off")
+					.appendTo($div);
+
 
 
 		var $button = $(document.createElement("a"))
@@ -395,12 +533,16 @@
 					.addClass("option_fields")
 					.addClass("inactive_fields")
 					.appendTo($div);
+		var id = 1;
 		$select.find("option").each(function() {
 			if ($(this).val() != "") {
 				var $option = $(document.createElement("div"))
 							.addClass("combobox_item")
 							.appendTo($fields)
-							.html($(this).val());
+							.html($(this).val())
+							.attr("id", $.fn.CBox.options["lineId"].replace(/{i}/, id));
+				id++;
+
 				if ($(this).attr("selected") == true) {
 					$option.addClass("selected");
 					$input.val($(this).val());
@@ -411,17 +553,36 @@
 		return $div;
 	}
 
+
 	/*
 	 * Replace select with combobox
 	 */
-	$.fn.replaceWithComboBox = function() {
+	$.fn.replaceWithComboBox = function(options) {
+		$.fn.CBox.options = $.extend($.fn.CBox.defaults, options);
 		this.each(function() {
 			$select_div = createComboBoxFromSelect($(this));
 			$combobox = new ComboBox();
-			$combobox.$select = $(this);
+			$combobox.$input = $(this);
+			$combobox.type   = "select";
 			$combobox.$select_div = $select_div;
 			$combobox.initComboBox($(this));
 		});
 	}
+	
+	$.ComboBox = function(options) {
+		$.fn.CBox.defaults["ajax"] = true;
+		$.fn.CBox.options = $.extend($.fn.CBox.defaults, options);
+		
+		$combobox = new ComboBox();
+		$select_div = $combobox.createComboBox();
+		$combobox.$select_div = $select_div;
+		var $input = $(document.createElement("input"))
+			.attr("type", "hidden")
+			.appendTo($select_div);
+		$combobox.$input = $input
+		$combobox.type = "hidden";
+		$combobox.initComboBox($input, "hidden");
+	}
+
 
 })(jQuery);
